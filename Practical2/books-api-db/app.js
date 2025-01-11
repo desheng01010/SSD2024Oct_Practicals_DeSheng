6c9623d6717fd83fcae04cb1462e13a73088b148
desheng01010 <162773776+desheng01010@users.noreply.github.com>
2025-01-11 15:24:47 +0800

    update Practical 3 & 4

--- Practical2/books-api-db/app.js
+++ Practical2/books-api-db/app.js
@@ -8,9 +8,15 @@ const validateBook = require("./middlewares/validateBook");
 const app = express();
 const port = 3000; // Use environment variable or default port
 
+const staticMiddleware = express.static("public"); // Path to the public folder
+
+const usersController = require("./controllers/usersController");
+
 // Include body-parser middleware to handle JSON data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+
+app.use(staticMiddleware); // Mount the static middleware
 
 // Routes for GET requests (replace with appropriate routes for update and delete later)
 app.get("/books", booksController.getAllBooks);
@@ -20,6 +26,15 @@ app.put("/books/:id", booksController.updateBook); // PUT for updating books
 app.delete("/books/:id", booksController.deleteBook); // DELETE for deleting books
 app.post("/books", validateBook, booksController.createBook); // POST for creating books (can handle JSON data)
 app.put("/books/:id", validateBook, booksController.updateBook);
+
+app.get("/users/search", usersController.searchUsers);
+app.get("/users/with-books", usersController.getUsersWithBooks);
+
+app.post("/users", usersController.createUser); // Create user
+app.get("/users", usersController.getAllUsers); // Get all users
+app.get("/users/:id", usersController.getUserById); // Get user by ID
+app.put("/users/:id", usersController.updateUser); // Update user
+app.delete("/users/:id", usersController.deleteUser); // Delete user
 
 app.listen(port, async () => {
   try {
